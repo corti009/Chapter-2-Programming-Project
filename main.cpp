cf14b7b8ffb3e913ef57671a359b4873c64926ae
corti009 <co28109@email.vccs.edu>
2025-07-31 02:26:15 +0000

    this code seems to work but gonna try something else to see if anything else changes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
+#include <iomanip> 
 
 const double ANNUAL_RISE_MM = 1.5;
+
+int main() {
+    double riseIn5Years;
+    double riseIn7Years;
+    double riseIn10Years;
+
+    riseIn5Years = ANNUAL_RISE_MM * 5;
+    riseIn7Years = ANNUAL_RISE_MM * 7;
+    riseIn10Years = ANNUAL_RISE_MM * 10;
+
+    
+
+    std::cout << "The ocean's level will be " << riseIn5Years << " millimeters higher in 5 years." << std::endl;
+    std::cout << "The ocean's level will be " << riseIn7Years << " millimeters higher in 7 years." << std::endl;
+    std::cout << "The ocean's level will be " << riseIn10Years << " millimeters higher in 10 years." << std::endl;
+
+    return 0;
+}
