3a4d7b9bbf6f455c83a1c63c9c9a3515ce4d3600
corti009 <co28109@email.vccs.edu>
2025-07-31 18:08:51 +0000

    This is my final draft for it. added fixed decimal precion for the output.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <iomanip> 
+#include <iomanip> // Required for std::fixed and std::setprecision
 
 const double ANNUAL_RISE_MM = 1.5;
 
@@ -12,7 +12,8 @@ int main() {
     riseIn7Years = ANNUAL_RISE_MM * 7;
     riseIn10Years = ANNUAL_RISE_MM * 10;
 
-   
+    
+    std::cout << std::fixed << std::setprecision(1);
 
     std::cout << "The ocean's level will be " << riseIn5Years << " millimeters higher in 5 years." << std::endl;
     std::cout << "The ocean's level will be " << riseIn7Years << " millimeters higher in 7 years." << std::endl;
