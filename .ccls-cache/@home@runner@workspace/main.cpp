7d815370282decc1a14e6dd8a7aa0f4652b5ba8e
corti009 <co28109@email.vccs.edu>
2025-07-31 03:00:30 +0000

    Trial code for Energy drinks.

---
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip> 
+#include <cmath>  
+
+const int TOTAL_CUSTOMERS = 16500;
+const double PURCHASE_PERCENTAGE = 0.15;
+const double CITRUS_PREFERENCE_PERCENTAGE = 0.58;
+
+int main() {
+    double customersPurchasingEnergyDrinks_double;
+    double customersPreferringCitrus_double;
+
+    customersPurchasingEnergyDrinks_double = TOTAL_CUSTOMERS * PURCHASE_PERCENTAGE;
+    customersPreferringCitrus_double = customersPurchasingEnergyDrinks_double * CITRUS_PREFERENCE_PERCENTAGE;
+
+   
+    std::cout << "The approximate number of customers in the survey purchasing one or more energy drinks per week: "
+              << customersPurchasingEnergyDrinks_double << std::endl;
+    std::cout << "The approximate number of customers in the survey preferring citrus-flavored energy drinks: "
+              << customersPreferringCitrus_double << std::endl;
+
+    return 0;
+}
